837589fb739edbd2aa120f51ff6e65f1df485afd
Gurinder Singh <gurinderrawala@gmail.com>
2023-07-23 14:27:04 -0400

    changed font

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -5,14 +5,11 @@ import HomePageIntro from "../Components/HomePage/HomePageIntro";
 import HomePagePart1 from "../Components/HomePage/HomePagePart1";
 import Projects from "../Components/HomePage/Projects";
 import ShowShow from "../Components/slideshow/";
-//import Slideshow from '../Components/slideshow';
-// import Slideshow from "../Components/SlideShow";
 
 
 const Home = () =>{
     return(
         <div style={{overflow: 'hidden'}}>
-            {/* <Slideshow /> */}
             <ShowShow />
             <HomePagePart1 />
             <DetailedIntro />
